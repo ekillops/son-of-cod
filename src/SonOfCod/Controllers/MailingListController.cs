69c372a7a18d40f22b7f6f00996237c340fc1ddb
Erik Killops <erik.killops@gmail.com>
2017-02-17 12:43:48 -0800

    Add MailingList controller with views

---
+++ src/SonOfCod/Controllers/MailingListController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using SonOfCod.Models;
+using Microsoft.AspNetCore.Authorization;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace SonOfCod.Controllers
+{
+    public partial class MailingListController : BaseController
+    {
+        public MailingListController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, ApplicationDbContext db) : base(userManager, signInManager, db)
+        {
+        }
+
+        public IActionResult Join()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Join(Contact newContact)
+        {
+            _db.Contacts.Add(newContact);
+            _db.SaveChanges();
+            return RedirectToAction("Index", "Home");
+        }
+
+        [Authorize]
+        public IActionResult List()
+        {
+            List<Contact> allContacts = _db.Contacts.ToList();
+            return View(allContacts);
+        }
+    }
+}
